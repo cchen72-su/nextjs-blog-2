cd707157a2e23f36fd98908aa1378b61b0b99c96
cchen72-su <cchen72@syr.edu>
2021-05-24 23:10:18 +0800

    static deploy

--- next.config.js
+++ next.config.js
@@ -3,3 +3,10 @@ module.exports = {
       domains: ['localhost'],
     },
 }
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : '',
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : '',
+};
